45cde133b749b3eeec8be0a5e9d92ea0231668c9
Farid Mahmudov <Razormad666@gmail.com>
2025-01-31 13:36:52 +0100

    update

---
+++ app/pages/index.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { NuxtLink } from '#components';
+
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <p>Current route: {{ route.path }}</p>
+    <NuxtLink to="https://nuxt.com/docs/getting-started/routing" target="_blank">Learn more about Nuxt Routing</NuxtLink>
+    <NuxtLink :to="{name: 'about'}">
+      Go to about
+    </NuxtLink>
+  </div>
+</template>
+
+<style scoped>
+
+  a {
+    display: block;
+  }
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2025-01-31',
+  future: {
+    compatibilityVersion: 4
+  },
   devtools: { enabled: true }
 })
