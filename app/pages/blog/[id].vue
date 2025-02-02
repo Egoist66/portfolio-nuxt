e674c8b3d39617d8022c1239c17a1dd687d8d96c
Farid Mahmudov <Razormad666@gmail.com>
2025-02-02 15:31:20 +0100

    update

---
+++ app/pages/blog/[id].vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+const route = useRouter()
+
+
+
+const H1 = defineNuxtComponent({
+    setup(props, context) {
+
+        console.log(props);
+    },
+    beforeMount() {
+        console.log(route);
+    },
+    render() {
+        return h('h1', 'Blog: ' + route.currentRoute.value.params.id)
+    },
+    props: {
+        id: String
+    }
+})
+
+</script>
+
+
+
+<template>
+    <div>
+
+        <H1 :id="route.currentRoute.value.params.id?.toString()" />
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
